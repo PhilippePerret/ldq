1be6a9187bc1d05d327394fc1333426341c8d702
PhilippePerret <philippe.perret@yahoo.fr>
2024-12-11 08:21:56 +0100

    Version 0.2.11

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule LdQ.MixProject do
   def project do
     [
       app: :ldq,
-      version: "0.2.10",
+      version: "0.2.11",
       elixir: "~> 1.14",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
