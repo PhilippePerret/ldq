c8531fb06a2e23d8e2d36186337efd0647e6e8b5
PhilippePerret <philippe.perret@yahoo.fr>
2024-12-13 08:33:51 +0100

    version 0.2.20 -- modification des sockets options

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule LdQ.MixProject do
   def project do
     [
       app: :ldq,
-      version: "0.2.19",
+      version: "0.2.20",
       elixir: "~> 1.14",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
