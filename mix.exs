aa61d1422a78e9c0b8f074f942612f0a3a158333
PhilippePerret <philippe.perret@yahoo.fr>
2024-12-13 08:37:18 +0100

    v 0.2.21 - suppression

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule LdQ.MixProject do
   def project do
     [
       app: :ldq,
-      version: "0.2.20",
+      version: "0.2.21",
       elixir: "~> 1.14",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
