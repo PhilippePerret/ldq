d89f07b17f4aa9bc076492e9a80eedb66ee38bc1
PhilippePerret <philippe.perret@yahoo.fr>
2025-06-10 08:53:41 +0200

    Nouvelles méthodes

---
+++ test/feature/book_evaluation/_book_evaluation_methods.ex
@@ -0,0 +1,29 @@
+defmodule BookEvaluationMethods do
+  @moduledoc """
+  Pour utiliser ces méthodes, placer : 
+
+    Code.require_file(Path.join(__DIR__, "_book_evaluation_methods.ex"))
+
+  … tout en haut du fichier.exs et :
+
+    import BookEvaluationMethods
+
+  … dans la définition du module de test
+  """
+
+  def get_book_of_proc(procedure) do
+    procedure.data["book_id"]
+    |> LdQ.Library.Book.get()
+  end
+
+  def get_user_of_proc(procedure) do
+    procedure.data["user_id"]
+    |> LdQ.Comptes.get_user!()
+  end
+
+  def get_author_of_proc(procedure) do
+    procedure.data["author_id"]
+    |> LdQ.Library.get_author!()
+  end
+
+end
