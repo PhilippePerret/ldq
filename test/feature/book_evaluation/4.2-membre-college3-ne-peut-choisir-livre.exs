55759ee437b3f74198c04588915506685b4fbe20
PhilippePerret <philippe.perret@yahoo.fr>
2025-06-10 08:56:19 +0200

    Nouveau test qui passe

--- test/feature/book_evaluation/4.2-membre-college3-ne-peut-choisir-livre.exs
+++ test/feature/book_evaluation/4.2-membre-college3-ne-peut-choisir-livre.exs
@@ -1,3 +1,5 @@
+Code.require_file(Path.join(__DIR__, "_book_evaluation_methods.ex"))
+
 defmodule LdQWeb.BookSubmissionTestsChoixLivrePerMembre2 do
   use LdQWeb.FeatureCase, async: false
 
@@ -6,14 +8,22 @@ defmodule LdQWeb.BookSubmissionTestsChoixLivrePerMembre2 do
   import TestHelpers
   import FeaturePublicMethods
 
+  import BookEvaluationMethods
+
   # @tag :skip
   test "Un membre de collège 3 ne peut pas choisir le livre" do
     %{parrain_id: parrain_id, procedure: procedure} = bddshot("evaluation-book/3-parrain-et-start-eval")
-    membre = get_membre_with_session(not: parrain_id, min_credit: LdQ.Evaluation.CreditCalculator.points_for(:seuil_college_two))
+    membre = get_membre_with_session(not: parrain_id, min_credit: LdQ.Evaluation.CreditCalculator.points_for(:seuil_college_three) + 1)
+
+    # Le livre concerné par cette évaluation
+    book    = get_book_of_proc(procedure)
 
     membre 
     |> se_connecte()
-    |> pause(4)
+    |> rejoint_la_page("/membre/#{membre.id}") # sa page d'accueil personnelle
+    |> pause(5)
+    |> et_voit("h4", "Nouveaux livres à évaluer")
+    |> et_ne_voit_pas("div", book.title)
     |> se_deconnecte()
 
 
